fe28594d465e188f37647f5762824c6e8148aacb
paulohenrique-lab <ph_adv17@hotmail.com>
2025-08-17 22:53:11 +0000

    operadores logicos

---
+++ # include <stdio.c
@@ -0,0 +1,46 @@
+# include <stdio.h>
+
+int main() {
+
+int a = 10, b = 5;
+
+
+if( a > 0 && b > 0){
+    printf("Os dois números são positivos\n");
+} else {
+    printf("Os números não são positivos\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+
+
+}
+
+
+
+
+
+
+
+
