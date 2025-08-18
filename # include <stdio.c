ceac10b1d10b4362c0b16c7aec4b95f7c32453af
paulohenrique-lab <ph_adv17@hotmail.com>
2025-08-18 03:09:24 +0000

    operadores logicos

--- # include <stdio.c
+++ # include <stdio.c
@@ -4,16 +4,24 @@ int main() {
 
 int a = 10, b = 5;
 
-
-if( a > 0 && b > 0){
+// &&(e) , ||(ou), !(negação) - segue a tabela verdade!
+if( a > 0 || b > 0){
     printf("Os dois números são positivos\n");
 } else {
-    printf("Os números não são positivos\n");
+    printf("Os dois numeros são negativos!\n");
 }
 
+if (a > 0 && b > 0) {
+    printf("Os números são positivos!\n");
+} else {
+    printf("Então, pelo menos um número é negativo!\n");
+}
 
-
-
+if (!a){
+    printf("A variável é negativa!");
+} else {
+    prinf("A variável é positiva!");
+}
 
 
 
